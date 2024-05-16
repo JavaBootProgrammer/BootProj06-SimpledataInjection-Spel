75811402be618ae23dca600110d7424c2292db2d
anand8533 <anand8533@gmail.com>
2024-05-16 15:03:00 -0400

    1

--- src/main/java/com/nt/sbeans/TestsRateCard.java
+++ src/main/java/com/nt/sbeans/TestsRateCard.java
@@ -1,33 +1,27 @@
 package com.nt.sbeans;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import lombok.Data;
 
 @Component("tInfo")
 @Data
 public class TestsRateCard {
-	@Value("${test.xray}")
-	private  float xrayRate;
-	@Value("${test.2decho}")
-	private float  echo2dRate;
-	@Value("${test.3decho}")
-	private float  echo3dRate;
-	@Value("${test.mri}")
-	private  float   mriRate;
-	
-	//toString()
-	@Override
-	public String toString() {
-		return "TestsRateCard [xrayRate=" + xrayRate + ", echo2dRate=" + echo2dRate + ", echo3dRate=" + echo3dRate
-				+ ", mriRate=" + mriRate + "]";
-	}
-	
-	
-	
-	
-	
-	
+    @Value("${test.xray}")
+    private float xrayRate;
+    @Value("${test.2decho}")
+    private float echo2dRate;
+    @Value("${test.3decho}")
+    private float echo3dRate;
+    @Value("${test.mri}")
+    private float mriRate;
+
+    //toString()
+    @Override
+    public String toString() {
+        return "TestsRateCard [xrayRate=" + xrayRate + ", echo2dRate=" + echo2dRate + ", echo3dRate=" + echo3dRate
+                + ", mriRate=" + mriRate + "]";
+    }
+
 
 }
